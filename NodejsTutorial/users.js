2adcd826798b293a4f1c47562956d3a8d6a831ea
PiyawatSritavong <piya.sritavong@gmail.com>
2021-06-10 23:53:30 +0700

    NodejsTutorial
    Node JS Test

---
+++ NodejsTutorial/users.js
@@ -0,0 +1,20 @@
+
+const users = [
+    {
+        id: 1,
+        name: 'Piyawat',
+        mail: 'piyawat@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Kamin',
+        mail: 'kamin@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Sira',
+        mail: 'sira@gmail.com'
+    }
+]
+
+module.exports = users;
